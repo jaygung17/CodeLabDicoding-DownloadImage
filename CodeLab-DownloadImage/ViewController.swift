d922630b660fba72dc3b1a7e6d0e88f2ec513346
Novan Agung Waskito <68317534+jaygung17@users.noreply.github.com>
2022-12-15 16:09:20 +0700

    Initial Commit

---
+++ CodeLab-DownloadImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodeLab-DownloadImage
+//
+//  Created by Novan Agung Waskito on 15/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
